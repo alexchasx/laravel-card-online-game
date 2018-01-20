43098325498388d67bf2e838ce2b7255f5c14c21
k241285@mail.ru <aleksandr.chasovnikov@gmail.com>
2018-01-20 21:54:25 +0300

    Расположение карт на поле боя (почти готово)

--- resources/views/card/battleground.blade.php
+++ resources/views/card/battleground.blade.php
@@ -12,62 +12,60 @@
             <div class="enemy-deck card-deck">enemy-deck</div>
             <div class="player-deck card-deck">player-deck</div>
 
-            <div class="card card-1">
-                <div class="attack">5</div>
-                <div class="health_points">4</div>
-                <div class="armor">3</div>
-                <div class="energy">2</div>
-                <div class="card_name">Наименование</div>
-                <div class="ability">Маскировка</div>
+            <div class="places card_places">
+                <div class="box enemy_hand">
+                    <div class="place player">
+                        @foreach ([1,2,3,4,5,6,7] as $number)
+                            <div class="card_place place_{{ $number }}"></div>
+                        @endforeach
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+                <div class="box enemy_place">
+                    <div class="place player">
+                        @foreach ([1,2,3,4,5,6,7] as $number)
+                            <div class="card_place place_{{ $number }}"></div>
+                        @endforeach
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+                <div class="box player_place">
+                    <div class="place player">
+                        @foreach ([1,2,3,4,5,6,7] as $number)
+                            <div class="card_place place_{{ $number }}"></div>
+                        @endforeach
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+                <div class="player_hand">
+                    @foreach ([1, 2, 3] as $number)
+                        <div class="card card-{{ $number }}">
+                            <div class="btn attack">5</div>
+                            <div class="btn health_points">4</div>
+                            <div class="btn armor">3</div>
+                            <div class="btn energy">2</div>
+                            <button class="btn card_name">
+                                Наименование
+                            </button>
+                            <button class="btn ability">
+                                Маскировка
+                            </button>
+                        </div>
+                    @endforeach
+                    <div class="clearfix"></div>
+                </div>
             </div>
-            <div class="card card-2">
-                <div class="attack">5</div>
-                <div class="health_points">4</div>
-                <div class="armor">3</div>
-                <div class="energy">2</div>
-                <div class="card_name">Наименование</div>
-                <div class="ability">Маскировка</div>
-            </div>
-            <div class="card card-3">
-                <div class="attack">5</div>
-                <div class="health_points">4</div>
-                <div class="armor">3</div>
-                <div class="energy">2</div>
-                <div class="card_name">Наименование</div>
-                <div class="ability">Маскировка</div>
-            </div>
-            <div class="card card-4">
-                <div class="attack">5</div>
-                <div class="health_points">4</div>
-                <div class="armor">3</div>
-                <div class="energy">2</div>
-                <div class="card_name">Наименование</div>
-                <div class="ability">Маскировка</div>
-            </div>
-            <div class="card card-5">
-                <div class="attack">5</div>
-                <div class="health_points">4</div>
-                <div class="armor">3</div>
-                <div class="energy">2</div>
-                <div class="card_name">Наименование</div>
-                <div class="ability">Маскировка</div>
-            </div>
-            <div class="card card-6">
-                <div class="attack">5</div>
-                <div class="health_points">4</div>
-                <div class="armor">3</div>
-                <div class="energy">2</div>
-                <div class="card_name">Наименование</div>
-                <div class="ability">Маскировка</div>
-            </div>
-            <div class="card card-7">
-                <div class="attack">5</div>
-                <div class="health_points">4</div>
-                <div class="armor">3</div>
-                <div class="energy">2</div>
-                <div class="card_name">Наименование</div>
-                <div class="ability">Маскировка</div>
-            </div>
+
+
+
+
+
+            {{--<div class="place enemy">--}}
+                {{--@foreach ([1,2,3,4] as $number)--}}
+                    {{--<div class="card_place place_{{ $number }}"></div>--}}
+                {{--@endforeach--}}
+                    {{--<div class="clearfix"></div>--}}
+            {{--</div>--}}
 
         </div>
         <!-- //container -->
