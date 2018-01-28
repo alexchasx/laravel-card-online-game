c5a3a586722c89f0ecb3e88022dd7059a5f2e687
k241285@mail.ru <aleksandr.chasovnikov@gmail.com>
2018-01-28 18:07:02 +0300

    Отрефакторил сущности в соответствии с новыми миграциями

---
+++ public/my/js/adaptive_menu_jQuery.js
@@ -0,0 +1,15 @@
+$(function() {
+
+	$('.menuToggle').on('click', function() {
+
+		$('.menu').slideToggle(300, function() {
+
+			if($(this).css('display') === 'none') {
+				$(this).removeAttr('style');
+			}
+
+		});
+
+	});
+
+});
